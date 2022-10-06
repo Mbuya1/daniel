820f6cf56f45f2183c5e69e24d099c3f6e0a9595
Mbuya1 <113966683+Mbuya1@users.noreply.github.com>
2022-10-06 12:19:15 +0300

    Add files via upload

---
+++ conversion from degrees celcius to farenheitt.cpp
@@ -0,0 +1,19 @@
+//program to convert degrees celcius to degrees farenheit
+#include<iostream>
+using namespace std;
+float farenheit(float farenheit);
+int main()
+{
+int f;
+cout<<"Enter temperature in celcius :";
+cin>>f;
+float degrees=0.5555*(f-32);
+cout<<"The temperature in farenheit is :"<<degrees;
+return 0;	
+}
+float farenheit(float farenheit)
+{
+	int f;
+	float degrees=0.5555*(f-32);
+	return farenheit;
+}
