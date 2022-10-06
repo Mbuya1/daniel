48899cde8b1b0242abf4f4b29fec2a6c8fb271f8
Mbuya1 <113966683+Mbuya1@users.noreply.github.com>
2022-10-06 12:24:28 +0300

    Add files via upload

---
+++ area of a circle.cpp
@@ -0,0 +1,19 @@
+//program to calculate the area of a circle using a function
+#include<iostream>
+using namespace std;
+float circle(int r);//function prototype
+int main()
+{
+int radius;
+cout<<"enter radius :";
+cin>>radius;
+float area=circle(radius);//calling function 
+cout<<"the area is :"<<area<<endl;
+return 0;
+}
+// function definition
+float circle(int r){
+	float area;
+	area=3.142*r*r;
+	return area;	
+}
