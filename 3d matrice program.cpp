061e814e7d7632936e4f32eff6d5bc85ee3439c4
Mbuya1 <113966683+Mbuya1@users.noreply.github.com>
2022-11-03 08:24:42 +0300

    3d array
    3d array

---
+++ 3d matrice program.cpp
@@ -0,0 +1,25 @@
+// program for 3d
+#include <iostream>
+using namespace std;
+int main()
+{
+	//variable declaration and definition
+	int a[2][3][3]={
+		{{1,2,3},{3,4,5},{6,7,8}},
+		{{10,11,12},{13,14,15},{16,17,18}}
+		};
+		int i,j,k;
+		for( i=0;i<2;i++)
+		{
+			// using nested loops to get info from the other groups
+			for(j=0;j<3;j++)
+			{
+				for( k=0;k<3;k++)
+				{
+					//printing out
+					cout<<a[i][j][k]<<" , ";
+					}
+	       	}
+		}
+return 0;
+  }
